1c77d8cdec2756e6a5fe1fcef257730057e9d969
DESKTOP-73ML54I\User <dimka2010@gmail.com>
2024-09-18 20:21:56 +0300

    create class DbContext

---
+++ eTicket/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> contextOptions)
+            :base(contextOptions)
+        {
+
+        }
+    }
+}
