7a93f2bcb368bd98bf1a808a7afa964daea2d81c
DESKTOP-73ML54I\User <dimka2010@gmail.com>
2024-09-19 20:44:09 +0300

    Adding first Controller

---
+++ eTicket/Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext dbContext;
+
+        public ActorsController(AppDbContext context)
+        {
+            dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            var data = dbContext.Actors.ToList();
+            return View();
+        }
+    }
+}
