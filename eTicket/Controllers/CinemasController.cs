5e09ddc7a18f3531f1d73edeb41a2dae53c60032
DESKTOP-73ML54I\User <dimka2010@gmail.com>
2024-09-19 20:53:35 +0300

    Adding Cinemas Controller

---
+++ eTicket/Controllers/CinemasController.cs
@@ -0,0 +1,25 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext dbContext;
+
+        public CinemasController(AppDbContext context)
+        {
+            dbContext = context;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            var data = await dbContext.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
