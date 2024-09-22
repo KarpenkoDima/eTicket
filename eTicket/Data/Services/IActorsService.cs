e42916f141e888662713a2f49b7b911ec36fcf8a
DESKTOP-73ML54I\User <dimka2010@gmail.com>
2024-09-22 20:12:57 +0300

    create IActorService

---
+++ eTicket/Data/Services/IActorsService.cs
@@ -0,0 +1,17 @@
+﻿using eTicket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor updateActor);
+        void Delete(int id);
+    }
+}
