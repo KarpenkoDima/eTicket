2c35625a03322b335aaf2072d1ee2a3575337cc6
DESKTOP-73ML54I\User <dimka2010@gmail.com>
2024-09-22 20:30:25 +0300

    Confiruration wour Service: AddScoped

--- eTicket/Startup.cs
+++ eTicket/Startup.cs
@@ -1,4 +1,5 @@
 using eTicket.Data;
+using eTicket.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,9 @@ namespace eTicket
         {
             //DbContext configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+
+            // my services configuration
+            services.AddScoped<IActorsService, ActorsService>();
 
             services.AddControllersWithViews();
         }
