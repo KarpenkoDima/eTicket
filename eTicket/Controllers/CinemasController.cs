88365b1498facc7a3721ba0e0b0d30033dc9d189
dimak <dimka2010@gmail.com>
2024-09-28 19:03:18 +0300

    Delete Cinema

--- eTicket/Controllers/CinemasController.cs
+++ eTicket/Controllers/CinemasController.cs
@@ -50,5 +50,46 @@ namespace eTicket.Controllers
 
             return View(cinemaDetails);
         }
+
+        // GET: cimeas/edit/id
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            return View(cinemaDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Logo, Name, Description")] Cinema cinema)
+        {
+            if (false == ModelState.IsValid)
+            {
+                return View(cinema);
+            }
+            if (cinema.Id == id)
+            {
+                await cinemasService.UpdateAsync(id, cinema);
+                return RedirectToAction("Index");
+            }
+            return View(cinema);
+        }
+
+        // GET: cimeas/delete/id
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            return View(cinemaDetails);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            await cinemasService.DeleteAsync(id); 
+            return RedirectToAction("Index");
+        }
     }
 }
